3ad75ec690b804853e9cd9910bc140f94a634a8b
A12M9x91 <a1.hdadashnejad1@gmail.com>
2023-08-08 21:54:09 +0330

    re

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,21 +24,24 @@ function Home() {
         ) as Promise<{ id: string; task: string; state: boolean }[]>
     )
   );
-  function toggle() {
+  function toggleTodo() {
     return "checked";
   }
 
-  async function deleteTask(taskId:string) {
+  async function deleteTask(taskId: string) {
     const response = await fetch(`http://localhost:3000/api/${taskId}`, {
-      method: "DELETE"
-    })
-    const data = await response
-    console.log(data)
-    location.reload()
+      method: "DELETE",
+    });
+    const data = await response;
+    console.log(data);
+    location.reload();
   }
-  
+
+  // work on this !
+  async function handleSaveEdit(taskId: string, newTitle: string) {}
+
   return (
-    <main>
+    <main className="list">
       <App />
       <ul>
         {data.map((showtasks) => (
@@ -47,17 +50,28 @@ function Home() {
               <input
                 type="checkbox"
                 checked={showtasks.state}
-                onChange={toggle}
+                onChange={toggleTodo}
               />
               <span className="text">{showtasks.task}</span>
             </label>
-            <button className="btn btn-danger" onClick={() => deleteTask(showtasks.id)}>
-              <span className="fas fa-trash">DEL</span>
-            </button>
+            <div className="Buttons">
+              <button
+                className="btn btn-danger"
+                onClick={() => deleteTask(showtasks.id)}
+              >
+                <span className="fas fa-trash">DEL</span>
+              </button>
+              <button
+                className="btn"
+                onClick={() => handleSaveEdit(showtasks.id, showtasks.task)}
+              >
+                <span className="fas fa-edit">EDIT</span>
+              </button>
+            </div>
           </li>
         ))}
       </ul>
     </main>
   );
 }
-export default dynamic(()=> Promise.resolve(Home), {ssr: false})
+export default dynamic(() => Promise.resolve(Home), { ssr: false });
